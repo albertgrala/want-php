931aa0906365438611f1a38e3d15e812a1daa32c
Albert Gracia <albertgrala@gmail.com>
2013-08-04 16:29:58 +0200

    Remove old tmpls folder

--- app/tmpls/joomla.component.2_5/site/views/{{name}}/tmpl/default.php
+++
@@ -1,8 +0,0 @@
-<?php
-defined( '_JEXEC' ) or die;
-
-$document = JFactory::getDocument();
-//$document->addStyleSheet(JURI::base() . 'media/com_{{name}}/css/com_{{name}}.css');
-
-?>
-<h1>{{Name}} title</h1>
